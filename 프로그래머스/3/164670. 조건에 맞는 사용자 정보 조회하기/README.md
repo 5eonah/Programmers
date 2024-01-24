f916721579483c9bf55fb3efb135a7d5bb500de5
5eonah <155696535+5eonah@users.noreply.github.com>
2024-01-24 11:00:24 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 조건에 맞는 사용자 정보 조회하기 - 164670 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 1월 3일 10:52:51
+2024년 1월 3일 11:0:21
 
 ### 문제 설명
 
--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,7 +1,7 @@
-SELECT DISTINCT u.user_id, u.nickname,
-       u.city||' '||u.street_address1||' '||u.street_address2 "전체주소",
-       SUBSTR(TLNO, 1, 3)||'-'||SUBSTR(TLNO,4, 4)||'-'||SUBSTR(TLNO, 8, 4)"전화번호"
+SELECT u.user_id, u.nickname,
+       CONCAT(u.city, ' ', u.street_address1, ' ', u.street_address2) "전체주소",
+       CONCAT(LEFT(TLNO,3), '-', SUBSTR(TLNO,4, 4), '-',RIGHT(TLNO,4)) "전화번호"
 FROM used_goods_board b JOIN used_goods_user u ON b.writer_id=u.user_id
-WHERE u.user_id IN (SELECT writer_id FROM used_goods_board 
-                    GROUP BY writer_id HAVING COUNT(writer_id)>=3)
-ORDER BY user_id DESC;
+GROUP BY 1
+HAVING COUNT(u.user_id)>=3
+ORDER BY 1 DESC
