aef22dfd4aed0d4aeded26c12ec6bb013d50b4a0
5eonah <155696535+5eonah@users.noreply.github.com>
2024-03-19 09:52:04 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,4 +1,4 @@
-SELECT c.car_id, c.car_type, (c.daily_fee*30)*((100-p.discount_rate)/100) as FEE
+SELECT c.car_id, c.car_type, (c.daily_fee*30)*(1-p.discount_rate/100) as FEE
 FROM CAR_RENTAL_COMPANY_CAR c JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY h
      ON c.car_id = h.car_id 
      JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
@@ -8,6 +8,6 @@ WHERE c.car_type IN ('세단', 'SUV')
       AND c.car_id NOT IN (SELECT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
                            WHERE TO_CHAR(start_date,'YYYY-MM-DD')<='2022-11-30' 
                                  AND TO_CHAR(end_date,'YYYY-MM-DD')>='2022-11-01')
-GROUP BY c.car_id, c.car_type, (c.daily_fee*30)*((100-p.discount_rate)/100)
-HAVING (c.daily_fee*30)*((100-p.discount_rate)/100) BETWEEN 500000 AND 2000000
-ORDER BY (c.daily_fee*30)*((100-p.discount_rate)/100) DESC, c.car_type, c.car_id
+GROUP BY c.car_id, c.car_type, (c.daily_fee*30)*(1-p.discount_rate/100)
+HAVING (c.daily_fee*30)*(1-p.discount_rate/100) BETWEEN 500000 AND 2000000
+ORDER BY (c.daily_fee*30)*(1-p.discount_rate/100) DESC, c.car_type, c.car_id
